502f15b62ce5da047a68bd2fd208161f6a4f283d
HaileyFYK <youngtoo175@gmail.com>
2026-07-13 17:35:53 -0400

    Update Controlling_mult_devices.py

--- Controlling_mult_devices.py
+++ Controlling_mult_devices.py
@@ -3,3 +3,16 @@ from zaber_motion.ascil import Connection
 import time
 import math
 
+with Connection.open_serial>port("COM3") as connection:
+    connection.enable_alters()
+
+    device_list = connection.detect_devices()
+    print("Found {} devices:".format(len(device_list)))
+
+    for device in device_list:
+        print("Homing all axes of device with address {}.".format(device.address))
+        device.all_axes.home()
+        
+
+
+
