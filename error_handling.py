0373f719eb447c6f5a5aa01daac5e125b352533e
HaileyFYK <youngtoo175@gmail.com>
2026-07-14 10:53:44 -0400

    Create error_handling.py

---
+++ error_handling.py
@@ -0,0 +1,26 @@
+from zaber_motion import Units, MovementFailedException, MotionLibException, RequestTimeoutException
+from zaber_motion.ascii import Connection
+
+with Connection.open_serial_port("COM3") as connection:
+    connection.enable_alters()
+
+    device_list = connection.detect_devices()
+    print("Found {} devices:".format(len(device_list)))
+
+    device = device_list[0]
+    axis = device.get_axis(1)
+    if not axis.is_homed():
+        axis.home()
+
+    try: 
+        axis.move_absolute(1, Units.LENGTH_MILLIMETRES)
+    except MotionLibException as err:
+        print(err)
+    except RequestTimeoutException as err:
+        print(err)
+    except MovementFailedException as err:
+        print(err.detail.reason)
+    
+
+
+    
