8adde24aac7d49af0d651eb887590d5baac2b28e
HaileyFYK <youngtoo175@gmail.com>
2026-07-14 10:10:25 -0400

    Update Arbitrary_unit_conversions.py

--- Arbitrary_unit_conversions.py
+++ Arbitrary_unit_conversions.py
@@ -4,7 +4,7 @@ from zaber_motion.ascii import Connection
 with Connection.open_serial_port("COM3") as connection:
     connection.enable_alters()
 
-    device_list = conneciton.detect_devices()
+    device_list = connection.detect_devices()
     print("Found {} devices:".format(len(device_list)))
 
     device = device_list[0]
@@ -12,4 +12,10 @@ with Connection.open_serial_port("COM3") as connection:
     axis = device.get_axis(1)
     if not axis.is_homed():
         axis.home()
-        
+
+    native_units = axis.settings.convert_to_native_units("pos", 1, Units.LENGTH_CENTIMETRES)
+    print("1 cm to native units: {}".format(native_units))
+
+    cm = axis.settings.convert_from_native_units("pos", native_units, Units.LENGTH_CENTIMETRES)
+    print("Native units back to cm: {}".format(cm))
+    
