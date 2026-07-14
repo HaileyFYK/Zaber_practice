0b1d334032d8cda462bc424ff9cafa9d71387253
HaileyFYK <youngtoo175@gmail.com>
2026-07-14 12:23:41 -0400

    t

--- G-code.py
+++ G-code.py
@@ -1,4 +1,23 @@
 from zaber_motion.gcode import Translator
 from zaber_motion.ascii import Connection
-from zaber_motion import Units
 
+with Connection.open_serial_port("COM3") as connection:
+    connection.enable_alters()
+
+    device_list = connection.detect_devices()
+    print("Found {} devices:".format(len(device_list)))
+
+    device = device_list[0]
+    
+    stream = device.stream.get_stream(1)
+    stream = setup_live(1, 2)
+
+    translator = Translator.setup(stream)
+
+    translator.translate("G28") #axis.home()
+    translator.translate("G0 X100") #x_axis.move_absolute(Units.LENGTH_MILLIMETRES,100)
+    translator.translate("G0 Y100") #y_axis.move_absolute(Units.LENGTH_MILLIMETRES,100)
+    translator.translate("G0 X0 Y0") #x_axis.move_absolute(Units.LENGTH_MILLIMETRES,0), y_axis.move_absolute(Units.LENGTH_MILLIMETRES,0)
+
+    translator.flush()
+    stream.disable()
